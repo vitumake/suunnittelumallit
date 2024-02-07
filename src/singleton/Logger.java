6996b3682b43552bf62e20207ee8b009fa934ffe
markus <mag3768@gmail.com>
2024-02-07 22:07:31 +0200

    singelton done

---
+++ src/singleton/Logger.java
@@ -0,0 +1,24 @@
+package singleton;
+
+public class Logger {
+    
+    private static Logger instance;
+
+    private Logger() {
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public static void log(String message) {
+        System.out.println("Log: " + message);
+    }
+
+    public static void error(String message) {
+        System.out.println("Error: " + message);
+    }
+}
---
+++ src/singleton/Main.java
@@ -0,0 +1,9 @@
+package singleton;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Logger.log("Hello");
+        Logger.error("World");
+    }
+}
